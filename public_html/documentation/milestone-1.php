505babefc0852843e16c20e45f7db0fc9e51e922
Half Mortise <HalfMortise@users.noreply.github.com>
2018-07-16 08:26:45 -0600

    Added LI logo, hyperlinked to LI; still having issues with internal linking to image

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -8,7 +8,7 @@
 	<body>
 		<h2>Purpose</h2>
 			<div class="brightness">
-				<img src="./images/logo.png">
+				<a href="https://www.linkedin.com/in/joycorsi"><img src="https://bootcamp-coders.cnm.edu/~ecorsi/my-pwp-joy/public_html/images/logo.png" width="250px" height="250px" class="center"></a>
 			</div>
 		<h2>Audience</h2>
 		<h2>Goal</h2>
