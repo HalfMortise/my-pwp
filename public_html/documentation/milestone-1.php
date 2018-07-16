0f22cfce3e912b76082b78d692029b059e751953
Half Mortise <HalfMortise@users.noreply.github.com>
2018-07-15 21:57:58 -0600

    Linked to stylesheet. Added CSS: brightness when hover

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -3,6 +3,15 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Milestone 1</title>
+		<link rel="stylesheet" href="style.css">
 	</head>
-	<body>This is the body</body>
+	<body>
+		<h2>Purpose</h2>
+			<div class="brightness">
+				<img src="./images/logo.png">
+			</div>
+		<h2>Audience</h2>
+		<h2>Goal</h2>
+		<h2>Persona</h2>
+	</body>
 </html>
