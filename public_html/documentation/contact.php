04031d7f5d96226976d0fca790ceb771268a9c67
Half Mortise <HalfMortise@users.noreply.github.com>
2018-07-18 13:58:57 -0600

    Created pages for About and Contact

---
+++ public_html/documentation/contact.php
@@ -0,0 +1,20 @@
+<!--Contact Page-->
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <title>Contact</title>
+      <link rel="stylesheet" href="style.css">
+   </head>
+   <body>
+      <!--Header navigation-->
+      <ul class="nav">
+         <li class="nav li"><a href="./milestone-1.php">Home</a></li>
+         <li class="nav li"><a class="nav a" href="./audience.php">Audience</a></li>
+         <li class="nav li"><a href="./persona.php">Persona</a></li>
+         <li class="nav li"><a href="./about.php">About</a></li>
+         <li class="nav li"><a href="./contact.php">Contact</a></li>
+      </ul>
+      <!--End header navigation-->
+   </body>
+</html>
--- public_html/documentation/persona.php
+++ public_html/documentation/persona.php
@@ -1,21 +1,33 @@
-/**
- *
- */
+<!--Persona Page-->
 <!DOCTYPE html>
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>Milestone 1</title>
+		<title>Persona</title>
 		<link rel="stylesheet" href="style.css">
 	</head>
 	<body>
+		<!--Header navigation-->
 		<ul class="nav">
-			<li class="nav li"><a href="/">Home</a></li>
-			<li class="nav li"><a class="nav a" href="/">Audience</a></li>
-			<li><a class="nav a" href="/">Goal</a></li>
-			<li><a href="/">Persona</a></li>
-			<li><a href="/">About</a></li>
-			<li><a href="/">Contact</a></li>
+			<li class="nav li"><a href="./milestone-1.php">Home</a></li>
+			<li class="nav li"><a class="nav a" href="./audience.php">Audience</a></li>
+			<li class="nav li"><a href="./persona.php">Persona</a></li>
+			<li class="nav li"><a href="./about.php">About</a></li>
+			<li class="nav li"><a href="./contact.php">Contact</a></li>
 		</ul>
-</body>
+		<!--End header navigation-->
+		<!--This persona reflects a notional early user of this site-->
+		<h2>Persona</h2>
+		<div>
+			<img class="img" src="./images/hr.jpg" width="200px">
+			<ul>Name: Laura Groves</ul>
+			<ul>Gender: Female</ul>
+			<ul>Age: 38</ul>
+			<ul>Profession: Hiring manager at software engineering firm</ul>
+		</div>
+		<p>User Story: Laura wants to see a portfolio (personal web page) of technical skills.</p>
+		<p>Attitude: Laura is very active online. She knows that she needs to be not only aware of candidates' online presence but also conducts a great amount of research</p>
+		<p>Likes: A well-put together site with substantial content that reflects good written communication skills as well as programming language skills.</p>
+		<p>Needs: Laura is a web-savvy user but without technical expertise, and who expects ease of access and use.  She doesn't care about the code on the back end, but would be happy to recommend the site to others if it provides a positive user experience.</p>
+	</body>
 </html>
