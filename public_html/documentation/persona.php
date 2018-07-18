7b26b5369dc54bde8b9d079564b7b3de83db0ca3
Half Mortise <HalfMortise@users.noreply.github.com>
2018-07-18 14:39:21 -0600

    Added Use Case to Persona

--- public_html/documentation/persona.php
+++ public_html/documentation/persona.php
@@ -29,5 +29,12 @@
 		<p>Likes: A well-put together site with substantial content that reflects good written communication skills as well as programming language skills.</p>
 		<p>Needs: Laura is a web-savvy user but without technical expertise, and who expects ease of access and use.  She doesn't care about the code on the back end, but would be happy to recommend the site to others if it provides a positive user experience.</p>
 		<p>Frustrations: The limitations of LinkedIn, being able to verify a candidate's technical skills.<br/>The pressure to hire a qualified candidate without sufficient information to review.</p>
+		<p>Technology comfort level : high</p>
+		<ul>
+			<li>Very comfortable with newest technology and able to browse websites with ease and efficiency.</li>
+			<li>Always gets the newest version iPhone: X iOS 11.x  (the company also provides her with a company iPhone).</li>
+			<li>At work, she uses a Dell XPS 13, which has the 8th Gen i7, Intel UHD Graphics 620, 16 GB RAM, 13.3" screen, and 1TB SSD storage.</li>
+		</ul>
+		<p>Use Case: When Laura is hiring new software developers, she reviews resumes and LinkedIn profiles, but she also regularly searches the web for candidates' additional online presence. Her project managers have let her know to be on the look-out for GitHub activity and websites that the candidates may have already authored, to include a personal website. To them, this reflects intiative, confidence in and a willingess to market one's skills, and full immersion into the industry.</p>
 	</body>
 </html>
