b6b8066e223877bf68e6685cb56d07ec3887b397
Half Mortise <HalfMortise@users.noreply.github.com>
2018-07-18 13:41:55 -0600

    Added header with navigation

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -8,6 +8,14 @@
 		<link rel="stylesheet" href="style.css">
 	</head>
 	<body>
+		<ul class="nav">
+			<li class="nav li"><a href="/">Home</a></li>
+			<li class="nav li"><a class="nav a" href="/">Audience</a></li>
+			<li><a class="nav a" href="/">Goal</a></li>
+			<li><a href="/">Persona</a></li>
+			<li><a href="/">About</a></li>
+			<li><a href="/">Contact</a></li>
+		</ul>
 		<a href="https://www.google.com" style="text-decoration:none;"><h2>Purpose</h2></a>
 			<div class="brightness">
 				<a href="https://www.linkedin.com/in/joycorsi"><img src="./images/logo.png" width="100px"></a>
@@ -15,15 +23,17 @@
 		<h2>Audience</h2>
 		<h2>Goal</h2>
 		<h2>Persona</h2>
-		<ul>Name: Laura Groves</ul>
-		<ul>Gender: Female</ul>
-		<ul>Age: 38</ul>
-		<ul>Profession: Hiring manager at software engineering firm</ul>
+		<div>
+		<img class="img" src="./images/hr.jpg" width="200px">
+			<ul>Name: Laura Groves</ul>
+			<ul>Gender: Female</ul>
+			<ul>Age: 38</ul>
+			<ul>Profession: Hiring manager at software engineering firm</ul>
+		</div>
 		<p>User Story: Laura wants to see a portfolio (personal web page) of technical skills.</p>
 		<p>Attitude: Laura is very active online. She knows that she needs to be not only aware of candidates' online presence but also conducts a great amount of research</p>
 		<p>Likes: A well-put together site with substantial content that reflects good written communication skills as well as programming language skills.</p>
 		<p>Needs: Laura is a web-savvy user but without technical expertise, and who expects ease of access and use.  She doesn't care about the code on the back end, but would be happy to recommend the site to others if it provides a positive user experience.</p>
 
-		<img src="./images/hr.jpg" width="200px">
 	</body>
 </html>
