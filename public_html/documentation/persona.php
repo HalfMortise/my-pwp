ce6c8adf6416fc5f2b60ccbc7d7af46a10ba8538
Half Mortise <HalfMortise@users.noreply.github.com>
2018-07-18 13:44:39 -0600

    Created persona page

---
+++ public_html/documentation/persona.php
@@ -0,0 +1,21 @@
+/**
+ *
+ */
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Milestone 1</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<ul class="nav">
+			<li class="nav li"><a href="/">Home</a></li>
+			<li class="nav li"><a class="nav a" href="/">Audience</a></li>
+			<li><a class="nav a" href="/">Goal</a></li>
+			<li><a href="/">Persona</a></li>
+			<li><a href="/">About</a></li>
+			<li><a href="/">Contact</a></li>
+		</ul>
+</body>
+</html>
