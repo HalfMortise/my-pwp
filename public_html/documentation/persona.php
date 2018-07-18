a7a0f2182a19e2d884c3b0400fdadae8b1418180
Half Mortise <HalfMortise@users.noreply.github.com>
2018-07-18 14:27:35 -0600

    Added content to Persona

--- public_html/documentation/persona.php
+++ public_html/documentation/persona.php
@@ -7,16 +7,15 @@
 		<link rel="stylesheet" href="style.css">
 	</head>
 	<body>
-		<!--Header navigation-->
+<!--Header navigation-->
 		<ul class="nav">
 			<li class="nav li"><a href="./milestone-1.php">Home</a></li>
-			<li class="nav li"><a class="nav a" href="./audience.php">Audience</a></li>
 			<li class="nav li"><a href="./persona.php">Persona</a></li>
 			<li class="nav li"><a href="./about.php">About</a></li>
 			<li class="nav li"><a href="./contact.php">Contact</a></li>
 		</ul>
-		<!--End header navigation-->
-		<!--This persona reflects a notional early user of this site-->
+<!--End header navigation-->
+<!--This persona reflects a notional early user of this site-->
 		<h2>Persona</h2>
 		<div>
 			<img class="img" src="./images/hr.jpg" width="200px">
@@ -25,9 +24,10 @@
 			<ul>Age: 38</ul>
 			<ul>Profession: Hiring manager at software engineering firm</ul>
 		</div>
-		<p>User Story: Laura wants to see a portfolio (personal web page) of technical skills.</p>
+		<p>User Story: Laura wants to see a portfolio (personal website) to verify technical skills.</p>
 		<p>Attitude: Laura is very active online. She knows that she needs to be not only aware of candidates' online presence but also conducts a great amount of research</p>
 		<p>Likes: A well-put together site with substantial content that reflects good written communication skills as well as programming language skills.</p>
 		<p>Needs: Laura is a web-savvy user but without technical expertise, and who expects ease of access and use.  She doesn't care about the code on the back end, but would be happy to recommend the site to others if it provides a positive user experience.</p>
+		<p>Frustrations: The limitations of LinkedIn, being able to verify a candidate's technical skills.<br/>The pressure to hire a qualified candidate without sufficient information to review.</p>
 	</body>
 </html>
