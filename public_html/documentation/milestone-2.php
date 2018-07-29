6ed1ef8ff85d4f2f70d5d820038b4dfb714a8c10
Half Mortise <HalfMortise@users.noreply.github.com>
2018-07-29 12:13:04 -0600

    Added content strategy content for Nav Menu

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -15,14 +15,15 @@
       </ul>
       <!--End header navigation-->
 		<ul class="nav">
-			<li class="nav li"><a name="Milestone-2">Milestone 2 Instructions</a></li>
+			<li class="nav li"><a name="Milestone-2">Milestone 2a Instructions</a></li>
 		</ul>
 		<p>Document - in detail - the content you plan to feature in each section of your site. Be sure to include any details regarding interactive features and functionality that you wish to include. You are not expected to have any actual content prepared at this time, rather only a plan for the kind of content you would like to feature.</p>
 		<p>Keep this section brief and succinct, yet detailed enough to guide your development phase. One paragraph per content section should be sufficient.</p>
-
+		<p>Milestone 2a: Due 2 Aug</p>
+		<p>Milestone 2b: Due TBA</p>
 		<!--Content Strategy-->
       <h3>Content Strategy</h3>
-      <p><strong>Nav Menu</strong>: </p>
+      <p><strong>Nav Menu</strong>: The navigation menu will contain five sections/anchors on the same static page: Home, About, Services, Portfolio, and Contact. For the mobile version, the navigation will be collapsed by default, in the top right corner, and will expand downward when selected. On the desktop version, it will be displayed inline-block and aligned to the right.</p>
       <p><strong>Header</strong>: </p>
       <p><strong>About Me</strong>: </p>
       <p><strong>Services</strong>: </p>
@@ -32,12 +33,20 @@
 		<!--End Content Strategy-->
 		<h3>Wireframes</h3>
 		<div class="brightness">
+			<!--Mobile Wireframes-->
          <a href="./images/mobile1.png" target="_blank">
             <img src="./images/mobile1.png" height="387px"></a>
 			<a href="./images/mobile2.png" target="_blank">
 				<img src="./images/mobile2.png" height="387"></a>
 			<a href="./images/mobile3.png" target="_blank">
 				<img src="./images/mobile3.png" height="387px"></a>
+			<!--Desktop Wireframes--><br />
+			<a href="./images/desktop1.png" target="_blank">
+				<img src="./images/desktop1.png" height="387px"></a><br />
+			<a href="./images/desktop1.png" target="_blank">
+				<img src="./images/desktop2.png" height="387"></a><br />
+			<a href="./images/desktop3.png" target="_blank">
+				<img src="./images/desktop3.png" height="387px"></a>
       </div>
    </body>
 </html>
