cdcd0e014cbba7848871fe9f3bc8ddb970256f73
Half Mortise <HalfMortise@users.noreply.github.com>
2018-08-06 16:23:53 -0600

    Added framework for navbar

--- public_html/index.php
+++ public_html/index.php
@@ -3,10 +3,72 @@
    <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <title>Welcome to Café au Lait</title>
-      <!--Bootstrap 4 styling-->
+      <title>Welcome to My Page</title>
+      <!--CSS Styling-->
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+      <link rel="stylesheet" href="style.css">
    </head>
    <body>
+
+      <div class="container">
+         <header class="blog-header py-3">
+            <div class="row flex-nowrap justify-content-between align-items-center">
+               <div class="col-4 pt-1">
+                  <a class="text-muted" href="#">Subscribe</a>
+               </div>
+               <div class="col-4 text-center">
+                  <a class="blog-header-logo text-dark" href="#">Large</a>
+               </div>
+               <div class="col-4 d-flex justify-content-end align-items-center">
+               </div>
+            </div>
+         </header>
+
+         <div class="nav-scroller py-1 mb-2">
+            <nav class="nav d-flex justify-content-end align-items-center">
+               <a class="p-2 text-muted" href="#">Home</a>
+               <a class="p-2 text-muted" href="#">About</a>
+               <a class="p-2 text-muted" href="#">Services</a>
+               <a class="p-2 text-muted" href="#">Portfolio</a>
+               <a class="p-2 text-muted" href="#">Contact</a>
+            </nav>
+         </div>
+
+
+
+
+
+
+      <!--Main Body-->
+
+      <div class="container">
+         <p class="">
+            This is where About Me will go.
+         </p>
+      </div>
+      <div class="container">
+         <p>
+            This is where Services will go.
+         </p>
+      </div>
+      <div class="container">
+         <div class="row">
+            <div class="col-md-4">
+               <p>
+               This is where Portfolio will go.
+               </p>
+            </div>
+            <div class="col-md-4">
+               <p>
+                  This is where Portfolio will go.
+               </p>
+            </div>
+            <div class="col-md-4">
+               <p>
+                  This is where Portfolio will go.
+               </p>
+            </div>
+         </div>
+      </div>
    </body>
 </html>
