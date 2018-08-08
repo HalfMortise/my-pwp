ffbe119fb6c19ddda98491e582492997a4e5a169
Half Mortise <HalfMortise@users.noreply.github.com>
2018-08-08 11:44:23 -0600

    Experimented with some fonts and settled on one

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -4,7 +4,7 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Milestone 1</title>
-		<link rel="stylesheet" href="../style.css">
+		<link rel="stylesheet" href="../css/style.css">
 	</head>
 	<body>
 
--- public_html/index.php
+++ public_html/index.php
@@ -6,8 +6,8 @@
       <title>Welcome to My Page</title>
       <!--CSS Styling-->
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-      <link rel="stylesheet" href="style.css">
-   </head>
+      <link rel="stylesheet" href="./css/style.css">
+		<link href="https://fonts.googleapis.com/css?family=Playfair+Display+SC" rel="stylesheet"> 	</head>
    <body>
 
 
@@ -21,10 +21,13 @@
 			<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
 				<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
 					<li class="nav-item active">
-						<a class="nav-link" href="#">Home</a>
+						<a class="nav-link" href="https://www.halfmortise.com/" target="_blank">Home</a>
 					</li>
 					<li class="nav-item">
 						<a class="nav-link" href="#">About</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Services</a>
 					</li>
 					<li class="nav-item">
 						<a class="nav-link" href="#">Portfolio</a>
@@ -39,51 +42,23 @@
 
 
 
-			<!-- CONTENT -->
-			<div class="sting_tm_content">
+<!--Images needed: header bg (laptop/code/porch/coffee), resume icon, svcs icon, consulting icon, wireframe icon, gears icon -->
 
-				<!-- HERO -->
-				<div class="sting_tm_section" id="home">
-					<div class="sting_tm_hero_header_particle jarallax" data-speed="0">
-						<div class="overlay"></div>
-						<div id="particles-js"></div>
-						<div class="container hero">
-							<div class="sting_tm_hero_title">
-								<p class="first"><span class="another">I Am</span> <span class="sting_tm_animation_text_word"></span></p>
-								<div class="social_icons">
-									<ul>
-										<li><a href="#">LinkedIn</a></li>
-										<li><a href="#">GitHub</a></li>
-										<li><a href="#">Facebook</a></li>
-									</ul>
-								</div>
-							</div>
-							<div class="sting_tm_discover_wrap anchor">
-						<span>
-							<a href="#about">Discover</a>
-						</span>
-							</div>
-						</div>
-						<div class="svg shape">
-							<svg x="0px" y="0px" viewBox="0 186.5 1920 113.5">
-								<polygon points="-30,300 355.167,210.5 1432.5,290 1920,198.5 1920,300"></polygon>
-							</svg>
-						</div>
-					</div>
-				</div>
-				<!-- /HERO -->
+
 
 
 
 
 
       <!--Main Body-->
+		<div>
+			<div class="container">
+				<p class="">
+					This is where <a href="https://www.google.com/" target="_blank">About Me</a> will go.
+				</p>
+			</div>
+		</div>
 
-      <div class="container">
-         <p class="">
-            This is where About Me will go.
-         </p>
-      </div>
       <div class="container">
          <p>
             This is where Services will go.
@@ -108,5 +83,11 @@
             </div>
          </div>
       </div>
+
+
+
+
+
+
    </body>
 </html>
