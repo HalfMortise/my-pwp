6631eff775a4ad8a673413c40ff777d7b5112120
Half Mortise <HalfMortise@users.noreply.github.com>
2018-08-08 09:55:23 -0600

    Updated navbar

--- public_html/index.php
+++ public_html/index.php
@@ -10,30 +10,68 @@
    </head>
    <body>
 
-      <div class="container">
-         <header class="blog-header py-3">
-            <div class="row flex-nowrap justify-content-between align-items-center">
-               <div class="col-4 pt-1">
-                  <a class="text-muted" href="#">Subscribe</a>
-               </div>
-               <div class="col-4 text-center">
-                  <a class="blog-header-logo text-dark" href="#">Large</a>
-               </div>
-               <div class="col-4 d-flex justify-content-end align-items-center">
-               </div>
-            </div>
-         </header>
 
-         <div class="nav-scroller py-1 mb-2">
-            <nav class="nav d-flex justify-content-end align-items-center">
-               <a class="p-2 text-muted" href="#">Home</a>
-               <a class="p-2 text-muted" href="#">About</a>
-               <a class="p-2 text-muted" href="#">Services</a>
-               <a class="p-2 text-muted" href="#">Portfolio</a>
-               <a class="p-2 text-muted" href="#">Contact</a>
-            </nav>
-         </div>
+	<!--NAVBAR-->
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<a class="navbar-brand" href="#">HalfMortise</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
 
+			<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+				<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">About</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Portfolio</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Contact</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	<!--/NAVBAR-->
+
+
+
+			<!-- CONTENT -->
+			<div class="sting_tm_content">
+
+				<!-- HERO -->
+				<div class="sting_tm_section" id="home">
+					<div class="sting_tm_hero_header_particle jarallax" data-speed="0">
+						<div class="overlay"></div>
+						<div id="particles-js"></div>
+						<div class="container hero">
+							<div class="sting_tm_hero_title">
+								<p class="first"><span class="another">I Am</span> <span class="sting_tm_animation_text_word"></span></p>
+								<div class="social_icons">
+									<ul>
+										<li><a href="#">LinkedIn</a></li>
+										<li><a href="#">GitHub</a></li>
+										<li><a href="#">Facebook</a></li>
+									</ul>
+								</div>
+							</div>
+							<div class="sting_tm_discover_wrap anchor">
+						<span>
+							<a href="#about">Discover</a>
+						</span>
+							</div>
+						</div>
+						<div class="svg shape">
+							<svg x="0px" y="0px" viewBox="0 186.5 1920 113.5">
+								<polygon points="-30,300 355.167,210.5 1432.5,290 1920,198.5 1920,300"></polygon>
+							</svg>
+						</div>
+					</div>
+				</div>
+				<!-- /HERO -->
 
 
 
