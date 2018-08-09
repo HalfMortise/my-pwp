e9f9eb8d127d00749d493f15739eddcaa87f9134
Half Mortise <HalfMortise@users.noreply.github.com>
2018-08-09 09:39:59 -0600

    Resized placeholder images; restructured Portfolio section

--- public_html/index.php
+++ public_html/index.php
@@ -10,45 +10,45 @@
 		<link href="https://fonts.googleapis.com/css?family=Playfair+Display+SC" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
 	</head>
+	<body>
 
-   <body>
 
+		<!--NAVBAR-->
 
-	<!--NAVBAR-->
-		<nav class="navbar navbar-expand-lg navbar-light bg-light font">
-			<a class="navbar-brand" href="#">HalfMortise</a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
+	<header>
+		<div>
+			<nav class="navbar navbar-expand-lg navbar-light bg-light font">
+				<a class="navbar-brand" href="#">HalfMortise</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
 
-			<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-				<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-					<li class="nav-item active">
-						<a class="nav-link" href="https://www.halfmortise.com/" target="_blank">Home</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">About</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Services</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Portfolio</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Contact</a>
-					</li>
-				</ul>
-			</div>
-		</nav>
+				<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+					<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+						<li class="nav-item active">
+							<a class="nav-link" href="https://www.halfmortise.com/" target="_blank">Home</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">About</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Services</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Portfolio</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Contact</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	</header>
 	<!--/NAVBAR-->
 
 
-
 <!--Images needed: header bg (laptop/code/porch/coffee), resume icon, svcs icon, consulting icon, wireframe icon, gears icon -->
-
-
-
 
 
 
@@ -63,10 +63,10 @@
 
 
 		<!--ABOUT Section-->
-		<section>
-			<div class="container-fluid justify-content-center font">
-				<div class="row">
-					<p class="justify-content-center">
+		<section class="font">
+			<div>
+				<div>
+					<p>
 						This is where <a href="https://www.google.com/" target="_blank">About Me</a> will go.
 					</p>
 				</div>
@@ -75,37 +75,52 @@
 
 
 		<!--SERVICES Section-->
-		<section>
-			<div class="container font">
-				<p>
+		<section class="font">
+			<div class="container">
+				<div class="row">
+					<p>
 					This is where Services will go.
-				</p>
+					</p>
+				</div>
 			</div>
 		</section>
 
 
 		<!--PORTFOLIO Section-->
-		<section>
-			<div class="container-fluid justify-content-center font">
+		<section class="py-5 font">
+			<div>
 				<div class="row">
 					<div class="col-md-4">
-						<p>
-							This is where Portfolio will go.
-						</p>
+						<img class="img-fluid d-block mx-auto mb-4" src="./images/placeholder.jpg" alt="">
+						<div class="card mb-4">
+							<div class="card-body">
+								<h5 class="card-title">Portfolio Item 1</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							</div>
+						</div>
 					</div>
 					<div class="col-md-4">
-						<p>
-							This is where Portfolio will go.
-						</p>
+						<img class="img-fluid d-block mx-auto mb-4" src="./images/placeholder.jpg" alt="">
+						<div class="card mb-4">
+							<div class="card-body">
+								<h5 class="card-title">Portfolio Item 2</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							</div>
+						</div>
 					</div>
 					<div class="col-md-4">
-						<p>
-							This is where Portfolio will go.
-						</p>
+						<img class="img-fluid d-block mx-auto mb-4" src="./images/placeholder.jpg" alt="">
+						<div class="card mb-4">
+							<div class="card-body">
+								<h5 class="card-title">Portfolio Item 3</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
 		</section>
+
 
 
 
