f9456c7d3433ae9242b62b340b18c97e56cac4b4
Half Mortise <HalfMortise@users.noreply.github.com>
2018-08-14 16:47:41 -0600

    Structured 'services' section into cards

--- public_html/index.php
+++ public_html/index.php
@@ -75,15 +75,44 @@
 
 
 		<!--SERVICES Section-->
-		<section class="font">
-			<div class="container">
-				<div class="row">
-					<p>
-					This is where Services will go.
-					</p>
+	<section class="container-fluid text-md-center font">
+		<div class="row">
+			<div class="col-sm-4">
+				<div class="card">
+					<img class="img-opacity" src="images/desktop.svg" width="25px" alt="" />
+
+					<div class="card-body">
+						<h6 class="card-title">UI/UX Design</h6>
+						<p class="card-text">Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</p>
+					</div>
 				</div>
 			</div>
-		</section>
+			<div class="col-sm-4">
+				<div class="card">
+					<img class="img-opacity" src="images/cogs.svg" width="30px" alt="" />
+
+					<div class="card-body">
+						<h6 class="card-title">Full-stack Web Development</h6>
+						<p class="card-text">Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-4">
+				<div class="card">
+					<img class="img-opacity" src="images/users.svg" width="30px" alt="" />
+
+					<div class="card-body">
+						<h6 class="card-title">Consulting</h6>
+						<p class="card-text">Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+
+
 
 
 		<!--PORTFOLIO Section-->
@@ -120,6 +149,15 @@
 				</div>
 			</div>
 		</section>
+
+
+
+
+		<!--CONTACT Section-->
+
+
+
+
 
 		<!--FOOTER Section-->
 		<section class="font-footer">
