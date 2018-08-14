d38fd58e93f41136163953a79f457a8c55ac2349
Half Mortise <HalfMortise@users.noreply.github.com>
2018-08-14 16:51:03 -0600

    Removed card borders

--- public_html/index.php
+++ public_html/index.php
@@ -78,9 +78,8 @@
 	<section class="container-fluid text-md-center font">
 		<div class="row">
 			<div class="col-sm-4">
-				<div class="card">
+				<div class="card border-light">
 					<img class="img-opacity" src="images/desktop.svg" width="25px" alt="" />
-
 					<div class="card-body">
 						<h6 class="card-title">UI/UX Design</h6>
 						<p class="card-text">Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</p>
@@ -88,9 +87,8 @@
 				</div>
 			</div>
 			<div class="col-sm-4">
-				<div class="card">
+				<div class="card border-light">
 					<img class="img-opacity" src="images/cogs.svg" width="30px" alt="" />
-
 					<div class="card-body">
 						<h6 class="card-title">Full-stack Web Development</h6>
 						<p class="card-text">Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.</p>
@@ -98,9 +96,8 @@
 				</div>
 			</div>
 			<div class="col-sm-4">
-				<div class="card">
+				<div class="card border-light">
 					<img class="img-opacity" src="images/users.svg" width="30px" alt="" />
-
 					<div class="card-body">
 						<h6 class="card-title">Consulting</h6>
 						<p class="card-text">Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.</p>
