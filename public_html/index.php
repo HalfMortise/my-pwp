f3ce4e865145773bb3da1b5b4172d46fc8137e19
Half Mortise <HalfMortise@users.noreply.github.com>
2018-08-15 14:54:13 -0600

    Worked out the jumbotron section

--- public_html/index.php
+++ public_html/index.php
@@ -61,9 +61,70 @@
 
 
 		<!--JUMBOTRON Section-->
-		<section>
+		<main role="main">
+			<section id="jumbotron" class="jumbotron font text-center">
+				<div class="container-fluid">
+					<h1 class="jumbotron-heading">WELCOME</h1>
+					<p class="lead text-muted">Maybe put a background image here?</p>
+					<p>
+						<a href="#" class="btn btn-secondary my-2">Go there</a>
+					</p>
+				</div>
+			</section>
+<!--END JUMBOTRON-->
 
-		</section>
+			<div class="album py-5 bg-light">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-md-4">
+							<div class="card mb-4 shadow-sm">
+								<img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+								<div class="card-body">
+									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+									<div class="d-flex justify-content-between align-items-center">
+										<div class="btn-group">
+											<button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+											<button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+										</div>
+										<small class="text-muted">9 mins</small>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="card mb-4 shadow-sm">
+								<img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+								<div class="card-body">
+									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+									<div class="d-flex justify-content-between align-items-center">
+										<div class="btn-group">
+											<button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+											<button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+										</div>
+										<small class="text-muted">9 mins</small>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="card mb-4 shadow-sm">
+								<img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+								<div class="card-body">
+									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+									<div class="d-flex justify-content-between align-items-center">
+										<div class="btn-group">
+											<button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+											<button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+										</div>
+										<small class="text-muted">9 mins</small>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</main>
 
 		<!--ABOUT SECTION HEADER-->
 		<section class="container-fluid text-md-center font">
