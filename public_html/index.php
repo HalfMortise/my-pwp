cdc2e29d966e663c5e2ef12d17a78d63cd1d9bd4
Half Mortise <HalfMortise@users.noreply.github.com>
2018-08-15 15:41:26 -0600

    Moved About Me to below Jumbotron

--- public_html/index.php
+++ public_html/index.php
@@ -73,6 +73,49 @@
 			</section>
 <!--END JUMBOTRON-->
 
+
+
+
+
+
+			<!--ABOUT SECTION HEADER-->
+			<section class="container-fluid text-md-center font">
+				<div class="row text-md-center">
+					<div class="col-md-12 justify-content-center">
+						<h4 class="px-5">
+							About Me
+						</h4>
+					</div>
+				</div>
+			</section>
+
+			<!--ABOUT Section-->
+			<section class="px-5 font">
+				<div>
+					<div>
+						<p>
+							This is where <a href="https://www.google.com/" target="_blank">About Me</a> will go.
+						</p>
+					</div>
+				</div>
+			</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <!--SERVICES SECTION-->
 			<div class="album py-5 bg-light">
 				<div class="container-fluid">
@@ -128,79 +171,13 @@
 			</div>
 		</main>
 
-		<!--ABOUT SECTION HEADER-->
-		<section class="container-fluid text-md-center font">
-			<div class="row text-md-center">
-				<div class="col-md-12 justify-content-center">
-					<h4 class="px-5">
-					About Me
-					</h4>
-				</div>
-			</div>
-		</section>
-
-		<!--ABOUT Section-->
-		<section class="px-5 font">
-			<div>
-				<div>
-					<p>
-						This is where <a href="https://www.google.com/" target="_blank">About Me</a> will go.
-					</p>
-				</div>
-			</div>
-		</section>
-
-		<!--SERVICES SECTION HEADER-->
-		<section class="py-5 font text-center">
-			<div class="container-fluid text-center">
-				<div class="jumbotron text-center">
-					<h4 class="display-5 px-5 text-center">Services</h4>
-					<div class="card col-sm-4 col-md-4 col-lg-4 my-5 p-5 ml-0 mx-auto">
-						<div class="text-center border-light"></div>
-					</div>
-					<div class="card col-sm-4 col-md-4 col-lg-4 my-5 p-5 ml-0 mx-auto">
-						<div class="text-center border-light"></div>
-					</div>
-					<div class="card col-sm-4 col-md-4 col-lg-4 my-5 p-5 ml-0 mx-auto">
-						<div class="text-center border-light"></div>
-					</div>
-				</div>
-			</div>
-		</section>
 
 
-		<!--SERVICES Section-->
-	<section class="container-fluid text-md-center font">
-		<div class="row">
-			<div class="col-sm-4">
-				<div class="card border-light">
-					<div class="card-body justify-content-center">
-						<img class="img-opacity" src="images/desktop.svg" width="25px" alt="" />
-						<h6 class="card-title">UI/UX Design</h6>
-						<p class="card-text">Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</p>
-					</div>
-				</div>
-			</div>
-			<div class="col-sm-4">
-				<div class="card border-light">
-					<div class="card-body justify-content-center">
-						<img class="img-opacity" src="images/cogs.svg" width="30px" alt="" />
-						<h6 class="card-title">Full-stack Web Development</h6>
-						<p class="card-text">Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.</p>
-					</div>
-				</div>
-			</div>
-			<div class="col-sm-4">
-				<div class="card border-light">
-					<div class="card-body justify-content-center">
-						<img class="img-opacity" src="images/users.svg" width="30px" alt="" />
-						<h6 class="card-title">Consulting</h6>
-						<p class="card-text">Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
+
+
+
+
+
 
 
 		<!--PORTFOLIO SECTION HEADER-->
@@ -213,8 +190,6 @@
 					<div class="col-md-12 justify-content-center">
 				</div>
 		</section>
-
-
 
 
 		<!--PORTFOLIO Section-->
@@ -255,8 +230,18 @@
 
 
 
-		<!--CONTACT Section-->
 
+
+
+
+
+
+
+
+
+
+
+		<!--CONTACT Section-->
 
 		<!--CONTACT SECTION HEADER-->
 		<section class="container-fluid text-md-center font">
@@ -290,19 +275,6 @@
 				</div>
 			</footer>
 		</section>
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
    </body>
