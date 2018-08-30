d70f241641fdb553e088b2a443558405726483df
Half Mortise <HalfMortise@users.noreply.github.com>
2018-08-30 10:40:51 -0600

    Reverting back to previous version because something broke; trying again...

--- public_html/index.php
+++ public_html/index.php
@@ -1,27 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-   <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <title>Welcome to My Page</title>
-      <!--CSS Styling-->
-
-		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>Welcome to My Page</title>
+		<!--CSS Styling-->
+		<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" >
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-
-		<!-- Your JavaScript Form Validator -->
-		<script src="js/form-validate.js"></script>
-
-		<script src='https://www.google.com/recaptcha/api.js'></script>
-
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 		<script src="https://www.google.com/recaptcha/api.js"></script>
 		<link href="https://fonts.googleapis.com/css?family=Playfair+Display+SC" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
 		<link href="./css/style.css" rel="stylesheet">
 		<script rel="script" src="./js/script.js"></script>
+		<script src="https://www.google.com/recaptcha/api.js"></script>
 
 	</head>
 	<body>
@@ -29,54 +22,54 @@
 
 		<!--NAVBAR-->
 
-	<header>
-		<div>
-			<nav class="navbar navbar-expand-lg navbar-light bg-light font">
-				<a class="navbar-brand" href="#">HalfMortise</a>
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
+		<header>
+			<div>
+				<nav class="navbar navbar-expand-lg navbar-light bg-light font">
+					<a class="navbar-brand" href="#">HalfMortise</a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
 
-				<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-					<ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-						<li class="nav-item active">
-							<a class="nav-link" href="https://www.halfmortise.com/" target="_blank">Home</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#about">About</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#services">Services</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#portfolio">Portfolio</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#contact">Contact</a>
-						</li>
-					</ul>
-				</div>
-			</nav>
-		</div>
-<!--/NAVBAR-->
-		<div class="title">
-			<h1>- WELCOME -</h1>
-			<p class="header-links"><a href="https://www.linkedin.com/in/joycorsi" target="_blank">Linkedin</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="https://www.github.com/halfmortise" target="_blank">Github</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="https://www.facebook.com/halfmortise" target="_blank">Facebook</a></p>
-		</div>
-	</header>
-
-
-<!--TO DO-->
-<!--PORTFOLIO: padding between cards-->
+					<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+						<ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+							<li class="nav-item active">
+								<a class="nav-link" href="https://www.halfmortise.com/" target="_blank">Home</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#about">About</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#services">Services</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#portfolio">Portfolio</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#contact">Contact</a>
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+			<!--/NAVBAR-->
+			<div class="title">
+				<h1>- WELCOME -</h1>
+				<p class="header-links"><a href="https://www.linkedin.com/in/joycorsi" target="_blank">Linkedin</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="https://www.github.com/halfmortise" target="_blank">Github</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="https://www.facebook.com/halfmortise" target="_blank">Facebook</a></p>
+			</div>
+		</header>
 
 
+		<!--TO DO-->
+		<!--PORTFOLIO: padding between cards-->
 
 
 
-<!--Main Body-->
 
 
-<!--ABOUT ME Section-->
+		<!--Main Body-->
+
+
+		<!--ABOUT ME Section-->
 		<main class="mt-3">
 
 
@@ -94,7 +87,7 @@
 					</p>
 				</div>
 			</section>
-<!--END ABOUT ME-->
+			<!--END ABOUT ME-->
 
 
 
@@ -108,57 +101,56 @@
 
 
 
-<!--SERVICES-->
+			<!--SERVICES-->
 
 
-<!--SERVICES SECTION HEADER-->
-	<section id="services" class="jumbotron container-fluid text-md-center font">
+			<!--SERVICES SECTION HEADER-->
+			<section id="services" class="jumbotron container-fluid text-md-center font">
 
-			<div class="font shadow-box album py-2 bg-light">
-				<div class="container-fluid justify-content-center text-center">
-					<div class="row">
-						<div class="col-md-12 justify-content-center py-4">
-							<h4>Services</h4>
-						</div>
-<!--/SERVICES SECTION HEADER-->
+				<div class="font shadow-box album py-2 bg-light">
+					<div class="container-fluid justify-content-center text-center">
+						<div class="row">
+							<div class="col-md-12 justify-content-center py-4">
+								<h4>Services</h4>
+							</div>
+							<!--/SERVICES SECTION HEADER-->
 
 
-						<div class="col-md-4 mb-2">
-							<div class="card-title mb-3 justify-content-center">
-								<img class="icons" src="./images/desktop.svg" alt="" />
-								<p class="justify-content-center">UI/UX Design</p>
-								<div class="card-body mb-4">
-									<p class="card-text text-center">Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</p>
+							<div class="col-md-4 mb-2">
+								<div class="card-title mb-3 justify-content-center">
+									<img class="icons" src="./images/desktop.svg" alt="" />
+									<p class="justify-content-center">UI/UX Design</p>
+									<div class="card-body mb-4">
+										<p class="card-text text-center">Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</p>
+									</div>
 								</div>
 							</div>
-						</div>
-						<div class="col-md-4 mb-4">
-							<div class="card-title mb-4">
-								<img class="icons" src="./images/cogs.svg" alt="" />
-								<p class="justify-content-center">Full-stack Web Development</p>
-								<div class="card-body mb-4">
-									<p class="card-text text-center">Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</p>
+							<div class="col-md-4 mb-4">
+								<div class="card-title mb-4">
+									<img class="icons" src="./images/cogs.svg" alt="" />
+									<p class="justify-content-center">Full-stack Web Development</p>
+									<div class="card-body mb-4">
+										<p class="card-text text-center">Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</p>
+									</div>
 								</div>
 							</div>
-						</div>
-						<div class="col-md-4 mb-4">
-							<div class="card-title mb-4">
-								<img class="icons" src="./images/users.svg" alt="" />
-								<p class="justify-content-center">Consulting</p>
-								<div class="card-body mb-4">
-									<p class="card-text text-center">Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</p>
+							<div class="col-md-4 mb-4">
+								<div class="card-title mb-4">
+									<img class="icons" src="./images/users.svg" alt="" />
+									<p class="justify-content-center">Consulting</p>
+									<div class="card-body mb-4">
+										<p class="card-text text-center">Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</p>
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</section>
+			</section>
 
-<!--/SERVICES SECTION-->
+			<!--/SERVICES SECTION-->
 
-	</main>
-
+		</main>
 
 
 
@@ -168,7 +160,8 @@
 
 
 
-<!--PORTFOLIO SECTION HEADER-->
+
+		<!--PORTFOLIO SECTION HEADER-->
 
 		<section id="portfolio" class="text-md-center font py-4">
 			<div class="container-fluid justify-content-center text-center">
@@ -180,7 +173,7 @@
 
 
 
-<!--PORTFOLIO Section-->
+		<!--PORTFOLIO Section-->
 		<section id="portfolio-content" class="portfolio-container font container-fluid">
 			<div class="card-body">
 				<img class="img-fluid d-block mx-auto" src="./images/placeholder.jpg" alt="" />
@@ -219,20 +212,20 @@
 
 
 
-<!--CONTACT Section-->
+		<!--CONTACT Section-->
 
-<!--CONTACT SECTION HEADER-->
+		<!--CONTACT SECTION HEADER-->
 		<section id="contact" class="container-fluid font justify-content-center text-center">
 			<div class="row py-5 text-md-center">
 				<div class="col-md-12">
 					<h4>
-					Contact
+						Contact
 					</h4>
 
 				</div>
 			</div>
 
-<!-- CONTACT -->
+			<!-- CONTACT -->
 
 			<div class="contact-form">
 
@@ -263,17 +256,15 @@
 							<textarea name="message" class="form-input-text form-control" rows="6" required="required"></textarea>
 						</div>
 
-						<div class="g-recaptcha" data-sitekey="https://bootcamp-coders.cnm.edu/~ecorsi/my-pwp-joy/public_html/index.php">
-
-						</div>
-
 						<div>
 							<p>
 								<button type="submit" class="btn btn-sm btn-outline-secondary">Send Message</button>
 							</p>
 						</div>
 
+						<div class="g-recaptcha" data-sitekey="https://bootcamp-coders.cnm.edu/~ecorsi/my-pwp-joy/public_html/index.php">
 
+						</div>
 
 					</div>
 				</form>
@@ -283,7 +274,7 @@
 
 
 
-<!--FOOTER Section-->
+		<!--FOOTER Section-->
 		<section id="footer" class="font-footer">
 			<footer class="bg-dark text-light text-center py-4">
 				<div class="container">
@@ -293,5 +284,5 @@
 		</section>
 
 
-   </body>
+	</body>
 </html>
