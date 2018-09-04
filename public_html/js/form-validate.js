476eeeb293f64e0c8f315a0090066a6fc995262c
Half Mortise <HalfMortise@users.noreply.github.com>
2018-09-04 14:54:12 -0600

    Drafted form-validate.js; added doc block with brief description

---
+++ public_html/js/form-validate.js
@@ -0,0 +1,10 @@
+/**
+ * jQuery Validate Function
+ *
+ * This function provides front-end validation for email form.
+ * If all tests set up here pass, the form data is AJAX submitted
+ * to the mailer.php file.
+ **
+ * @author HalfMortise <halfmortise@protonmail.com>
+ **/
+
