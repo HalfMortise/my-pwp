c0e676885a7d0af736752477b326dc5fb95e27ff
Half Mortise <HalfMortise@users.noreply.github.com>
2018-09-04 14:50:40 -0600

    Added in connectivity to swiftmailer; added doc blocks and comments; added exception throws and messages

--- public_html/php/mailer.php
+++ public_html/php/mailer.php
@@ -35,4 +35,76 @@ try {
    $company = filter_input(INPUT_POST, "company", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
    $message = filter_input(INPUT_POST, "message", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 
+   //create Swift message
+   $swiftMessage = new Swift_Message();
+
+   /**
+    * Attached the sender to the message;
+    * This takes the form of an associative array,
+    * where $email is the key for the real name.
+    **/
+
+   $swiftMessage->setFrom([$email => $name]);
+
+   /**
+    * Attach the recipients to the message;
+    * $MAIL_RECIPIENTS is set in mail-config.php
+    **/
+
+   $recipients = $MAIL_RECIPIENTS;
+   $swiftMessage->setTo($recipients);
+
+   // attach the subject line to the message
+
+   $swiftMessage->setSubject($subject);
+
+
+   /**
+    * Attach the actual message to the message.
+    *
+    * Here we set two versions of the message: the HTML formatted message and a
+    * special filter_var()'d version of the message that generates a plain text
+    * version of the HTML content.
+    *
+    * One is used to display the entire $confirmLink to plain text;
+    * this lets users who aren't viewing HTML content in emails still access your
+    * links.
+    **/
+   $swiftMessage->setBody($message, "text/html");
+   $swiftMessage->addPart(html_entity_decode($message), "text/plain");
+
+   /**
+    * Send the email via SMTP. The SMTP server here is configured to relay
+    * everything upstream via your web host.
+    *
+    * This default may or may not be available on all web hosts;
+    * consult their documentation/support for details.
+    *
+    * SwiftMailer supports many different transport methods; SMTP was chosen
+    * because it's the most compatible and has the best error handling.
+    *
+    * @see http://swiftmailer.org/docs/sending.html Sending Messages - Documentation - Switftmailer
+    **/
+
+   $smtp = new Swift_SmtpTransport("localhost", 25);
+   $mailer = new Swift_Mailer($smtp);
+   $numSent = $mailer->send($swiftMessage, $failedRecipients);
+
+   /**
+    * The send method returns the number of recipients that accepted the Email.
+    * If the number attempted !== number accepted it's an Exception.
+    **/
+
+   if($numSent !== count($recipients)) {
+      // The $failedRecipients parameter passed in the send() contains an array of the Emails that failed.
+      throw(new RuntimeException("Unable to send email"));
+   }
+
+   // reports a successful send
+
+   echo "<div class=\"alert alert-success\" role=\"alert\">Email sent.</div>";
+
+   // reports email failure to send
+} catch(Exception $exception) {
+   echo "<div class=\"alert alert-danger\" role=\"alert\">Unable to send email: " . $exception->getMessage() . "</div>";
 }
